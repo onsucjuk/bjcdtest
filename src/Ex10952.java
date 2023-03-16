e520e7a8b87a926c651f80990870eceaa284c0af
JeongukChoi <onsucjuk@gmail.com>
2023-03-16 09:56:39 +0900

    bj10952

---
+++ src/Ex10952.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Ex10952 {
+        public static void main(String[] args) {
+
+            Scanner ad = new Scanner(System.in);
+
+            while(true) {
+
+                int a = ad.nextInt();
+                int b = ad.nextInt();
+
+                if (a==0 && b==0) {
+
+                    break;
+                }
+
+                System.out.println(a+b);
+
+            }
+
+        }
+    }
+
+
